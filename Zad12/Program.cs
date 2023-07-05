8bbcb9083ae46307c3557f8c42ab0ad8bc3eef2f
Сергей <volkov_sv97@mail.ru>
2023-07-05 12:22:21 +0400

    3

---
+++ Zad12/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int Prompt (string message)
+
+{
+    System.Console.Write (message) ;
+     string value = Console.ReadLine ();
+     int result = Convert.ToInt32 (value) ;
+     return result;
+}
+int number = Prompt ("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine ("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число'{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Второе число '{secondRank}'");
